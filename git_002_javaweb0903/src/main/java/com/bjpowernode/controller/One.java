c3bf24f03f3e2112ed2625389140ac0d04a2c3f7
Administrator <zar@qq.com>
2022-09-03 16:57:36 +0800

    项目经理完成了One类的增加

---
+++ git_002_javaweb0903/src/main/java/com/bjpowernode/controller/One.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.controller;
+
+/**
+ *
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("项目经理新增加的类.............");
+    }
+}
