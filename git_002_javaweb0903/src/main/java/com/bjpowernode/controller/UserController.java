8e7fc8891f87194f3b214593733a25f527802094
Administrator <zar@qq.com>
2022-09-03 15:42:51 +0800

    项目经理第一次将工程提交到本地仓库.

---
+++ git_002_javaweb0903/src/main/java/com/bjpowernode/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.controller;
+
+/**
+ *
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("项目经理开发的web应用程序..............");
+    }
+}
